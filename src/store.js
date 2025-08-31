9a14ebe5b1e1c597b15eec92178f1653f3b79e49
jonny <dmswnl93@naver.com>
2025-08-31 17:03:05 +0900

    feat.store

--- src/store.js
+++ src/store.js
@@ -7,3 +7,12 @@ export const userAtom = atom({
   gender: "",
   bio: "",
 });
+
+
+export const questionsAtom = atom([
+  { id: 1, q: "요즘 카메라 앱을 켜게 만드는 것", a: "" },
+  { id: 2, q: "9월, 가장 많이 떠오르는 생각", a: "" },
+  { id: 3, q: "지금 듣고 싶은 앨범", a: "" },
+  { id: 4, q: "나를 부끄럽게(shame) 만드는 순간", a: "" },
+  { id: 5, q: "지금 가고 싶은 식당", a: "" },
+]);
